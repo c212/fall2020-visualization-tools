6190aba6a4f0df4d58fb081b89f09bc7e50e476d
c212 <40395648+c212@users.noreply.github.com>
2020-10-07 16:15:32 -0400

    multiple circles in an array list

--- mouse-events/Screen.java
+++ mouse-events/Screen.java
@@ -1,12 +1,25 @@
 import javax.swing.JComponent; 
 import java.awt.Graphics;
 import java.awt.Color; 
+import java.util.ArrayList;
 
 public class Screen extends JComponent {
-  
-  Circle c = new Circle(100, 100, 80, Color.BLUE); 
-  
+  ArrayList<Circle> circles; // declaration  
+  public Screen(int number) {
+    this.circles = new ArrayList<Circle>(); // allocation 
+    for (int i = 0; i < number; i++) {
+      Circle c = new Circle((int)(Math.random() * 500) + 50,
+                            (int)(Math.random() * 500) + 50, 
+                            (int)(Math.random() * 100) + 20, 
+                             new Color((int)(Math.random()*256), 
+                                       (int)(Math.random()*256),
+                                       (int)(Math.random()*256))); 
+      this.circles.add( c ); // initialization of the array list one by one 
+        
+    }
+  }  
   public void paintComponent(Graphics g) {
-    this.c.draw(g); 
+    for (Circle c : this.circles) 
+      c.draw(g);
   }
 }
--- mouse-events/Wednesday.java
+++ mouse-events/Wednesday.java
@@ -4,7 +4,7 @@ public class Wednesday {
   public static void main(String[] args) {
     JFrame a = new JFrame("Circles"); 
     // System.out.println( a ); 
-    a.add( new Screen() ); 
+    a.add( new Screen(6) ); 
     a.setSize(600, 600); 
     a.setVisible(true); 
   }
